6d4665903d16cd570a1d9870ea807b8f488b6e29
José Luis Tello <jltello96@gmail.com>
2022-11-05 19:22:49 -0600

    adds

--- router/uber.py
+++ router/uber.py
@@ -3,7 +3,8 @@ from fastapi import APIRouter, status, Response
 
 
 router = APIRouter(
-    prefix='/companies'
+    prefix='/companies',
+    tags=['uber']
 )
 
 
@@ -12,11 +13,29 @@ def create_employee(mail: str, password: str, response: Response):
     email = 'pierre@palenca.com'
     passwordd = 'MyPwdChingon123'
     """
-    Simulates tocken autentication
+    Simulates incorrect username or password
     """
     if ((mail not in email) and (password not in passwordd)):
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {'error': f'CREDENTIALS_INVALID details: Incorrect username or password'}
+        return {'error': f'CREDENTIALS_INVALID', 'details': f'Incorrect username or password'}
     else:
         response.status_code = status.HTTP_200_OK
-    return  {'message': f'SUCCESS Access Token: cTV0aWFuQ2NqaURGRE82UmZXNVBpdTRxakx3V1F5'}
+    return  {'message': f'SUCCESS', 'Access Token': f'cTV0aWFuQ2NqaURGRE82UmZXNVBpdTRxakx3V1F5'}
+
+
+
+
+@router.get('/user', status_code=status.HTTP_200_OK)
+def get_employee_information(token: str, response: Response):
+    access_token = 'cTV0aWFuQ2NqaURGRE82UmZXNVBpdTRxakx3V1F5'
+    """
+    Simulates incorrect token
+    """
+    if token not in access_token:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'CREDENTIALS_INVALID', 'details': f'Incorrect token'}
+    else:
+        response.status_code = status.HTTP_200_OK
+    return  {'message': f'SUCCESS',
+            'platform': f"uber",
+            'profile': f'...'}
