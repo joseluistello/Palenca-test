cc9ad8b89b110aa127b89942a7374342bb81b968
José Luis Tello <jltello96@gmail.com>
2022-11-05 19:15:50 -0600

    adds

--- router/uber.py
+++ router/uber.py
@@ -3,8 +3,20 @@ from fastapi import APIRouter, status, Response
 
 
 router = APIRouter(
-    prefix='/compaies'
+    prefix='/companies'
 )
 
 
-
+@router.get('/login', status_code=status.HTTP_200_OK)
+def create_employee(mail: str, password: str, response: Response):
+    email = 'pierre@palenca.com'
+    passwordd = 'MyPwdChingon123'
+    """
+    Simulates tocken autentication
+    """
+    if ((mail not in email) and (password not in passwordd)):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'CREDENTIALS_INVALID details: Incorrect username or password'}
+    else:
+        response.status_code = status.HTTP_200_OK
+    return  {'message': f'SUCCESS Access Token: cTV0aWFuQ2NqaURGRE82UmZXNVBpdTRxakx3V1F5'}
